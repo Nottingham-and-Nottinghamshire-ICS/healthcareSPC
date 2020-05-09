1249d32e045da944fe31bef13ffeb598ba3d2baa
Chris Beeley <chris.beeley@gmail.com>
2020-05-09 10:52:28 +0100

    Click to zoom facet

--- R/mod_facet_graph.R
+++ R/mod_facet_graph.R
@@ -11,16 +11,16 @@ mod_facet_graph_ui <- function(id){
   ns <- NS(id)
   tagList(
     h4("Click a plot to see a bigger version"),
-    plotOutput(ns("facetPlot"), height = 1000, click = "plot_click")
+    plotOutput(ns("facetPlot"), height = 1000, click = ns("plot_click"))
   )
 }
-    
+
 #' facet_graph Server Function
 #'
 #' @noRd 
 mod_facet_graph_server <- function(input, output, session, title, variable){
   ns <- session$ns
- 
+  
   output$facetPlot <- renderPlot({
     
     open_data %>% 
@@ -36,11 +36,25 @@ mod_facet_graph_server <- function(input, output, session, title, variable){
                      scales = "free_y",
                      ncol = 5)
   })
+  
+  observeEvent(input$plot_click, {
+    
+    showModal(
+      modalDialog(
+        plotOutput(ns("zoomedGraph")),
+        size = "l")
+    )
+  })
+  
+  output$zoomedGraph <- renderPlot({
+    
+    open_data %>% 
+      dplyr::group_by(Date) %>% 
+      dplyr::count(.data[[variable]]) %>% 
+      dplyr::ungroup() %>% 
+      tidyr::complete(Date, .data[[variable]], fill = list(n = 0)) %>%
+      dplyr::filter(.data[[variable]] == input$plot_click$panelvar1) %>% 
+      qicharts2::qic(Date, n,
+                     data = .)
+  })
 }
-    
-## To be copied in the UI
-# 
-    
-## To be copied in the server
-# 
- 
