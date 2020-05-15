3ee1875cda910611d142bc669c0eda36523bdf03
Chris Beeley <chris.beeley@gmail.com>
2020-05-15 16:53:12 +0100

    Draw comparison graphs using individual modules

--- R/mod_single_double_graph.R
+++ R/mod_single_double_graph.R
@@ -10,18 +10,8 @@
 mod_single_double_graph_ui <- function(id){
   ns <- NS(id)
   tagList(
-    fluidRow(
-      column(3,
-             selectInput(ns("categoryOne"), "Type of data",
-                         choices = c("Acuity" = "Acuity_SNOMED",
-                                     "Chief complaint" = "ChiefComplaint_SNOMED",
-                                     "Grouped diagnosis" = "Diagnosis1_Grouped_SNOMED")),
-             uiOutput(ns("renderThingOne"))
-      ),
-      column(9,
-             plotOutput(ns("plotOne"))
-      )
-    )
+    mod_draw_comparison_graph_ui(ns("draw_comparison_graph_ui_1")),
+    mod_draw_comparison_graph_ui(ns("draw_comparison_graph_ui_2"))
   )
 }
 
@@ -31,44 +21,6 @@ mod_single_double_graph_ui <- function(id){
 mod_single_double_graph_server <- function(input, output, session){
   ns <- session$ns
   
-  # return the facets that they can plot
-  
-  plotFacets <- reactive({
-    
-    variable <- input$categoryOne
-
-    open_data %>%
-      dplyr::filter(!is.na(.data[[variable]])) %>%
-      dplyr::arrange(.data[[variable]]) %>%
-      dplyr::pull(.data[[variable]]) %>%
-      unique(.)
-    
-  })
-  
-  output$renderThingOne <- renderUI({
-    
-    selectInput(session$ns("thingOne"), 
-                "Select categories", 
-                choices = plotFacets(),
-                multiple = TRUE)
-  })
-  
-  output$plotOne <- renderPlot({
-    
-    validate(need(input$thingOne, "Please select a category"))
-    
-    variable <- input$categoryOne
-    
-    open_data %>% 
-      dplyr::group_by(Date) %>% 
-      dplyr::count(.data[[variable]]) %>% 
-      dplyr::ungroup() %>% 
-      dplyr::rename("Incident" = .data[[variable]]) %>% 
-      tidyr::complete(Date, Incident, fill = list(n = 0)) %>% 
-      dplyr::filter(Incident %in% input$thingOne) %>% 
-      qicharts2::qic(Date, n,
-                     data = .,
-                     title = knitr::combine_words(input$thingOne),
-                     chart = "c")
-  })
+  callModule(mod_draw_comparison_graph_server, "draw_comparison_graph_ui_1")
+  callModule(mod_draw_comparison_graph_server, "draw_comparison_graph_ui_2")
 }
