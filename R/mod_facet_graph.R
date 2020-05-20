a281b507b83050e42f52d41189ea3ab1697616c7
Chris Beeley <chris.beeley@gmail.com>
2020-05-20 15:43:53 +0100

    All working, merging to master

--- R/mod_facet_graph.R
+++ R/mod_facet_graph.R
@@ -21,68 +21,64 @@ mod_facet_graph_ui <- function(id){
 mod_facet_graph_server <- function(input, output, session, title, variable, sigma_choices){
   ns <- session$ns
   
+  # produce reactive which knows how many choices are showing if sigma is selected
+  
+  plotFacets <- reactive({
+    
+    plot_variables <- list_outputs[[variable]]$data %>% 
+      dplyr::filter(sigma.signal) %>% 
+      dplyr::group_by(facet1) %>% 
+      dplyr::count(sigma.signal)
+    
+    if(sigma_choices$sigmaTrue()){
+      
+    plot_variables <- plot_variables %>% 
+      dplyr::filter(n >= sigma_choices$sigmaLevel())
+    }
+    
+    plot_variables <- plot_variables %>%
+      dplyr::pull(facet1)
+  })
+  
   output$dynamicPlot <- renderUI({
     
     # calculate height of plot
     
-    number_of_plots <- length(unique(open_data[[variable]]))
+    number_of_plots <- length(unique(plotFacets()))
     
-    plot_height <- ceiling(number_of_plots / 5) * 200
+    plot_height <- ceiling(number_of_plots / 5) * 300
     
     plotOutput(session$ns("facetPlot"), height = plot_height, click = ns("plot_click"))
   })
-
-  facetReactive <- reactive({
-    
-    open_data %>% 
-      dplyr::group_by(Date) %>% 
-      dplyr::count(.data[[variable]]) %>% 
-      dplyr::ungroup() %>% 
-      dplyr::rename("Incident" = .data[[variable]]) %>% 
-      tidyr::complete(Date, Incident, fill = list(n = 0)) %>% 
-      qicharts2::qic(Date, n,
-                     data = .,
-                     title = title,
-                     chart = "c",
-                     facet = ~ Incident,
-                     scales = "free_y",
-                     ncol = 5)
-  })
-  
-  facetDebounce <- facetReactive %>%
-    debounce(1000)
-  
-  # pull into reactive and debounce
   
   output$facetPlot <- renderPlot({
     
-    final_data <- facetDebounce()$data
+    final_data <- list_outputs[[variable]]$data
     
     # filter by sigma if that's what they want
     
-      if(sigma_choices$sigmaTrue()){
-        
-        plotVariables <- facetDebounce()$data %>% 
-          dplyr::filter(sigma.signal) %>% 
-          dplyr::group_by(facet1) %>% 
-          dplyr::count(sigma.signal) %>% 
-          dplyr::filter(n >= sigma_choices$sigmaLevel()) %>%
-          dplyr::pull(facet1)
-        
-        final_data <- final_data %>%
-          dplyr::filter(facet1 %in% plotVariables)
-      }
-
-    final_data %>%
-      qicharts2::qic(x, y,
-                     data = .,
-                     title = title,
-                     chart = "c",
-                     freeze = which(seq(min(open_data$Date), max(open_data$Date), by = "day") == as.POSIXct("2020-03-23")),
-                     part.labels = c("Pre lockdown", "Lockdown"),
-                     facet = ~ facet1,
-                     scales = "free_y",
-                     ncol = 5)
+    if(sigma_choices$sigmaTrue()){
+      
+      final_data <- final_data %>%
+        dplyr::filter(facet1 %in% plotFacets())
+      
+      return(
+        final_data %>%
+          qicharts2::qic(x, y,
+                         data = .,
+                         title = title,
+                         chart = "c",
+                         freeze = which(seq(min(open_data$Date), max(open_data$Date), by = "day") == as.POSIXct("2020-03-23")),
+                         part.labels = c("Pre lockdown", "Lockdown"),
+                         facet = ~ facet1,
+                         scales = "free_y",
+                         ncol = 5)
+      )
+      
+    } else {     # or just return the pre-cached plot
+      
+     return(list_outputs[[variable]]) 
+    }
   })
   
   observeEvent(input$plot_click, {
